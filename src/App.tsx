d8c6b2fa8e149fd510ec9b643d74cac36ff1cda2
TheHeiseDev <meizuset@gmail.com>
2023-03-11 11:27:11 +0300

    Настроил lazy loading страниц

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import FullFurniture from "./pages/FullFurniture/FullFurniture";
-import FullOrder from "./pages/FullOrder.jsx/FullOrder";
 import Home from "./pages/Home";
 import MainLayout from "./layout/MainLayout";
+import Loader from "./components/UI/Loader/Loader";
+
+const FullFurniture = lazy(
+  () =>
+    import(/* webpachChunkName: "FullFurniture" */ "./pages/FullFurniture/FullFurniture")
+);
+const FullOrder = lazy(
+  () => import(/* webpachChunkName: "FullOrder" */ "./pages/FullOrder.jsx/FullOrder")
+);
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="/order" element={<FullOrder />} />
-        <Route path="/furniture/:id" element={<FullFurniture />} />
+        <Route
+          path="/order"
+          element={
+            <Suspense fallback={<Loader />}>
+              <FullOrder />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/furniture/:id"
+          element={
+            <Suspense fallback={<Loader />}>
+              <FullFurniture />
+            </Suspense>
+          }
+        />
         <Route path="*" element={<Home />} />
       </Route>
     </Routes>
