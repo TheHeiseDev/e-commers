5a38fbcc774d486c98d3c0310b734a2958821b27
TheHeiseDev <meizuset@gmail.com>
2023-03-19 19:52:57 +0300

    Добавил кнопку показать/скрыть пароль

--- src/components/UI/Form/Form.tsx
+++ src/components/UI/Form/Form.tsx
@@ -4,6 +4,8 @@ import Loader from "../Loader/Loader";
 import AuthAlert from "../Alerts/AuthAlert/AuthAlert";
 import { useSelector } from "react-redux";
 import { selectUserError } from "../../../store/slice/userSlice/userSlice";
+import { BsEyeFill } from "react-icons/bs";
+import { BsEyeSlashFill } from "react-icons/bs";
 
 interface IForm {
   title: string;
@@ -13,23 +15,31 @@ interface IForm {
 export const Form = ({ title, handleClick, loading }: IForm) => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const error = useSelector(selectUserError);
 
   return (
     <div className="form-auth__wrapper">
       <AuthAlert title={error?.message} display={error?.status} />
       <input
+        className="form-auth__email"
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="email"
       />
-      <input
-        type="password"
-        value={pass}
-        onChange={(e) => setPass(e.target.value)}
-        placeholder="password"
-      />
+      <div className="form-auth__pass">
+        <input
+          type={showPassword ? "text" : "password"}
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          placeholder="password"
+        />
+        <div onClick={() => setShowPassword(!showPassword)} className="form__svg">
+          {showPassword ? <BsEyeSlashFill /> : <BsEyeFill />}
+        </div>
+      </div>
+
       <button
         className={loading ? "active" : ""}
         onClick={() => handleClick(email, pass)}
