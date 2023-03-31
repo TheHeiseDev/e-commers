621ec7e6dfcf4ce515479be9f63f0582f0433603
TheHeiseDev <meizuset@gmail.com>
2023-03-31 12:50:36 +0300

    Добавил комментарий в индекс

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { store } from "./store/store";
 import { Provider } from "react-redux";
 import "./services/firebase";
 
-// root
+// root test 2
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Provider store={store}>
